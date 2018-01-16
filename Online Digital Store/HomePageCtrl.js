b13e5f102e2cd34540d4fe8b58897f39aeca230c
AkshathaKShetty <31784177+AkshathaKShetty@users.noreply.github.com>
2018-01-16 10:55:33 +0530

    Add files via upload

---
+++ Online Digital Store/HomePageCtrl.js
@@ -0,0 +1,88 @@
+﻿var myHome = angular.module("myHome", []);
+
+//Service to get JSON Data
+myHome.service('getDataService', function ($http) {
+    this.getallproducts = function () {
+        return $http.get('Products.json');
+    }
+});
+
+//Directive for Tile View
+//myHome.directive("tileView", function (getDataService) {
+//    return {
+//        restrict: 'C',
+//        templateUrl: 'TileView.html',
+//        link: function (scope, elem, attr) {
+//            scope.products = getDataService.products;
+//        }
+//    }
+//});
+
+
+myHome.controller('HomePageCtrl', function ($scope, getDataService) {
+    getDataService.getallproducts().then(function (resp) {
+        $scope.products = resp.data;
+        $scope.count = $scope.products.length;
+    });
+
+
+//Search Module
+    $scope.search = function (item) {
+        if ($scope.searchText == undefined) {
+            return true;
+        }
+        else {
+            if (item.name.toLowerCase()
+                         .indexOf($scope.searchText.toLowerCase()) != -1 ||
+                item.cost.toLowerCase()
+                         .indexOf($scope.searchText.toLowerCase()) != -1||
+                item.rating.toLowerCase()
+                         .indexOf($scope.searchText.toLowerCase()) != -1) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+
+
+//Filter Module
+
+    $scope.priceincludes = [];
+    $scope.ranges = [];
+    debugger;
+    $scope.includeprice = function (pricerange) {
+        var i = pricerange.includes($scope.priceincludes);
+        
+        if (i =="true") {
+            $scope.priceincludes.splice(i, 1);
+            ranges = pricerange.split(',').splice(i, 1);
+        } else {
+            $scope.priceincludes.push(pricerange);
+        }
+        var arraystring = $scope.priceincludes.join();
+        var rangearray = arraystring.split(',')
+        $scope.maxrange = function (rangearray) {
+            return Math.max.apply(math, rangearray);
+        };
+        $scope.minrange = function (rangearray) {
+            return Math.min.apply(math, rangearray);
+        };
+        $scope.ranges[1] = $scope.maxrange(rangearray);
+        $scope.ranges[0] = $scope.minrange(rangearray);
+        console.log($scope.ranges);
+    }
+
+    $scope.pricefilter = function (item) {
+        if ($scope.item.length > 0) {
+            if ((parseint(item.cost) >= parseint($scope.ranges[0])) && (parseint(item.cost) <= parseint($scope.ranges[1])))
+                return item;
+        }
+    }
+
+});
+
+
+
+
---
+++ Online Digital Store/app.js
@@ -0,0 +1,15 @@
+﻿var myApp = angular.module("myApp", ['ui.router', 'myHome']);
+myApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+         .state('Home', {
+             url: '/Home',
+             templateUrl: 'HomePage.html',
+             controller: 'HomePageCtrl'
+         })
+        .state('Home.Filter', {
+            url: '/Filter',
+            templateUrl: 'Filter.html',
+            controller: 'HomePageCtrl'
+        });
+    $urlRouterProvider.otherwise('/Home');
+}]);
